452fd8e6449920e505a3bd235d2f8f239a688c0b
MarianoImende <98611190+MarianoImende@users.noreply.github.com>
2023-10-10 13:14:16 -0300

    Update genSaldo.py

--- genSaldo.py
+++ genSaldo.py
@@ -6,7 +6,7 @@ locale.setlocale(locale.LC_ALL, 'C.UTF-8')
 # Función para generar un JSON con un saldo aleatorio en formato de dinero
 def generar_json_saldo():
     saldo = random.uniform(100, 1000000)  # Generar un número aleatorio entre 100 y 1,000,000
-    saldo_formateado = format_currency(saldo, 'USD', locale='es_AR')  # Formatear el número como dinero en dólares (puedes ajustar la moneda si es diferente)
+    saldo_formateado = format_currency(saldo, 'ARS', locale='es_AR')  # Formatear el número como dinero en dólares (puedes ajustar la moneda si es diferente)
 
     data = {
         "saldo": saldo_formateado
