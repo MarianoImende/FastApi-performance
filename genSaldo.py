81a2c427c83c6631954f5eb89cbc75444a77a8f3
MarianoImende <98611190+MarianoImende@users.noreply.github.com>
2023-10-10 13:09:31 -0300

    Update genSaldo.py

--- genSaldo.py
+++ genSaldo.py
@@ -1,6 +1,8 @@
 import random
 from babel.numbers import format_currency
 
+locale.setlocale(locale.LC_ALL, 'C.UTF-8')
+
 # Función para generar un JSON con un saldo aleatorio en formato de dinero
 def generar_json_saldo():
     saldo = random.uniform(100, 1000000)  # Generar un número aleatorio entre 100 y 1,000,000
